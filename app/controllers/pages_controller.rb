c1b2e0b1bb5042fffbba9290f93748a4f4b2d8f5
Tejas Dinkar <tejas@gja.in>
2011-11-26 23:42:41 +0530

    Added a / route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  def show
-    @page = Page.find_by_name(params[:page_name])
+  def show_page(page_name)
+    @page = Page.find_by_name(page_name)
     render @page.template
+  end
+
+  def show
+    show_page(params[:page_name])
   end
 
   def update
@@ -9,5 +13,13 @@ class PagesController < ApplicationController
     @page.update_sections(JSON.parse(params[:content]))
     @page.save!
     render text: ""
+  end
+
+  def root
+    if Page.exists?(:name => "root")
+      show_page("root")
+    else
+      redirect_to "/admin"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Argand::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  root :to => "pages#root"
   get ':page_name' => "pages#show"
   post ':page_name' => "pages#update"
 end
