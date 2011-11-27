debf8f5a345255b4c04738783b3b2a0edc1dae1f
Tejas Dinkar <tejas@gja.in>
2011-11-27 10:38:45 +0530

    Added a readme in the dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,9 +1,15 @@
 ActiveAdmin::Dashboards.build do
+  section :magic_pages do
+    ul do
+      li "A page named root will be shown as the home page"
+      li "A page named not_found will be shown if no page can be found"
+    end
+  end
 
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
-  
+
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
   #
@@ -14,7 +20,7 @@ ActiveAdmin::Dashboards.build do
   #       end
   #     end
   #   end
-  
+
   # == Render Partial Section
   # The block is rendered within the context of the view, so you can
   # easily render a partial rather than build content in ruby.
@@ -24,7 +30,7 @@ ActiveAdmin::Dashboards.build do
   #       render 'recent_posts' # => this will render /app/views/admin/dashboard/_recent_posts.html.erb
   #     end
   #   end
-  
+
   # == Section Ordering
   # The dashboard sections are ordered by a given priority from top left to
   # bottom right. The default priority is 10. By giving a section numerically lower
